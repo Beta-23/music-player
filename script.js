bb9dce7367befa5658190268d4fe9fedcd18196d
Beta-23 <alfred.dominguez@gmail.com>
2021-01-05 22:30:21 -0800

    bulid button class and attribute functionality

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ let isPlaying = false;
 // Play Btn
 function playSong() {
     isPlaying = true;
+    playButton.classList.replace('fa-play', 'fa-pause');
+    playButton.setAttribute('title', 'Pause');
     music.play();
 }
 
 // Pause Btn
 function pauseSong() {
     isPlaying = false;
+    playButton.classList.replace('fa-pause', 'fa-play');
+    playButton.setAttribute('title','Play');
     music.pause();
 }
 
