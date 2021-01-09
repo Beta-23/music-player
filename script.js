10c33886b6c62c34248a098bbd8964837538bc47
Beta-23 <alfred.dominguez@gmail.com>
2021-01-08 22:11:22 -0800

    Create the music library obj

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+
+const image = document.querySelector('img');
+const title = document.querySelector('title');
+const artist = document.querySelector('artist');
 const music = document.querySelector('audio');
 const prevButton = document.getElementById('prev')
 const playButton = document.getElementById('play');
 const nextButton = document.getElementById('next');
+
+// Music library
+const songs = [
+    {
+        name: 'ad-1',
+        displayName: 'Album 1',
+        artist: 'Artist 1'
+    },
+    {
+        name: 'ad-2',
+        displayName: 'Album 2',
+        artist: 'Artist 2'
+    },
+    {
+        name: 'ad-3',
+        displayName: 'Album 3',
+        artist: 'Artist 3'
+    },
+    {
+        name: 'rhythmic-1',
+        displayName: 'rhythmic-1',
+        artist: 'rhythmic-artist'
+    }
+
+];
+
 
 // Check if playing
 let isPlaying = false;
@@ -25,4 +55,5 @@ function pauseSong() {
 // Play or Pause event listener
 playButton.addEventListener('click', () => (
     isPlaying ? pauseSong() : playSong()));
+
 
